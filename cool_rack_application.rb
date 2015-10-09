b341fc12d0792e3b8ad164900f09035e1945b7bd
Matt Abhay <matt.abhay@vibes.com>
2015-10-09 18:42:47 -0500

    Ex3:Building middleware
    $ ruby cool_rack_application.rb
    
    $ curl -X PATCH localhost:9292
    $ curl -X GET localhost:9292

--- cool_rack_application.rb
+++ cool_rack_application.rb
@@ -2,6 +2,7 @@
 require "rack"
 
 class CoolRackApplication
+  # The required instance method for EVERY rack app.
   def call(env)
     request = Rack::Request.new(env)
     http_verb = request.request_method
@@ -15,5 +16,28 @@ class CoolRackApplication
   end
 end
 
+# This middleware will detect all PATCH requests and 405 them.
+class PatchBlockingMiddleware
+  def initialize(app)
+    @app = app # The final rack app that this middleware can pass a request to
+  end
+
+  # Notice that this middle ware is a rack app too!
+  def call(env)
+    request = Rack::Request.new(env)
+
+    if request.patch?
+      [405, {}, ["PATCH requests not allowed!\n"]]
+    else
+      @app.call(env)
+    end
+  end
+end
+
 # Run on localhost, port 9292
-Rack::Handler::WEBrick.run CoolRackApplication.new, Port: 9292
+app = Rack::Builder.new do # combines middlewares and your final rack app
+  use PatchBlockingMiddleware
+  run CoolRackApplication.new
+end
+
+Rack::Handler::WEBrick.run app, Port: 9292
